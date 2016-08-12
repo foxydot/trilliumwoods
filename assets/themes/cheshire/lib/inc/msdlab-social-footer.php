c79bc79b65ccd74194ed624411e1bd4ae571415f
Sara Carbaugh <sara@saracarbaugh.com>
2016-08-12 09:32:00 -0400

    added LEED logo

--- assets/themes/cheshire/lib/inc/msdlab-social-footer.php
+++ assets/themes/cheshire/lib/inc/msdlab-social-footer.php
@@ -28,7 +28,7 @@ function msdlab_do_social_footer(){
     print '<div class="row">';
     print '<div class="social">'.$copyright.' '.$address.'</div>';
     print '<nav class="footer-menu" itemtype="http://schema.org/SiteNavigationElement" itemscope="itemscope" role="navigation">'.$footer_menu.'</nav>';
-    print '<div class="icons"><i class="icon icon-equal-housing"></i><i class="icon fa fa-wheelchair"></i><i class="logo logo-lcs"></i></div>';
+    print '<div class="icons"><i class="icon icon-equal-housing"></i><i class="icon fa fa-wheelchair"></i><i class="logo logo-leed"></i><i class="logo logo-lcs"></i></div>';
     print '</div>';
     //print '<div class="backtotop"><a href="#pre-header"><i class="fa fa-angle-up"></i></a></div>';
 }
